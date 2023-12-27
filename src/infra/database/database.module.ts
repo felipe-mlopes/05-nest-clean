4392c3e5ced67b9effcb36112a29cf92fa52e806
Felipe Lopes <felipe.mlopes03@gmail.com>
2023-12-26 21:43:12 -0300

    refactor: switch to infra folder

---
+++ src/infra/database/database.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common'
+
+import { PrismaService } from './prisma/prisma.service'
+import { PrismaQuestionsRepository } from './prisma/repositories/prisma-questions-repository'
+import { PrismaQuestionCommentsRepository } from './prisma/repositories/prisma-question-comments-repository'
+import { PrismaQuestionAttachmentRepository } from './prisma/repositories/prisma-question-attachment-repository'
+import { PrismaAnswersRepository } from './prisma/repositories/prisma-answers-repository'
+import { PrismaAnswerCommentsRepository } from './prisma/repositories/prisma-answer-comments-repository'
+import { PrismaAnswerAttachmentRepository } from './prisma/repositories/prisma-answer-attachment-repository'
+
+@Module({
+  providers: [
+    PrismaService,
+    PrismaQuestionsRepository,
+    PrismaQuestionCommentsRepository,
+    PrismaQuestionAttachmentRepository,
+    PrismaAnswersRepository,
+    PrismaAnswerCommentsRepository,
+    PrismaAnswerAttachmentRepository,
+  ],
+  exports: [PrismaService],
+})
+export class DatabaseModule {}
