7be4981f6df9b59b15c48d8fe1cd51ad29446159
Felipe Lopes <felipe.mlopes03@gmail.com>
2023-12-27 23:00:59 -0300

    feat: add prisma repositories in providers + Questions & Students repositories in exports

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -7,17 +7,36 @@ import { PrismaQuestionAttachmentRepository } from './prisma/repositories/prisma
 import { PrismaAnswersRepository } from './prisma/repositories/prisma-answers-repository'
 import { PrismaAnswerCommentsRepository } from './prisma/repositories/prisma-answer-comments-repository'
 import { PrismaAnswerAttachmentRepository } from './prisma/repositories/prisma-answer-attachment-repository'
+import { QuestionsRepository } from 'src/domain/forum/application/repositories/questions-repository'
+import { StudentsRepository } from 'src/domain/forum/application/repositories/students-repository'
+import { PrismaStudentsRepository } from './prisma/repositories/prisma-students-repository'
 
 @Module({
   providers: [
     PrismaService,
-    PrismaQuestionsRepository,
+    {
+      provide: QuestionsRepository,
+      useClass: PrismaQuestionsRepository,
+    },
+    {
+      provide: StudentsRepository,
+      useClass: PrismaStudentsRepository,
+    },
     PrismaQuestionCommentsRepository,
     PrismaQuestionAttachmentRepository,
     PrismaAnswersRepository,
     PrismaAnswerCommentsRepository,
     PrismaAnswerAttachmentRepository,
   ],
-  exports: [PrismaService],
+  exports: [
+    PrismaService,
+    QuestionsRepository,
+    StudentsRepository,
+    PrismaQuestionCommentsRepository,
+    PrismaQuestionAttachmentRepository,
+    PrismaAnswersRepository,
+    PrismaAnswerCommentsRepository,
+    PrismaAnswerAttachmentRepository,
+  ],
 })
 export class DatabaseModule {}
