21a5b1e594c5038b20e9bb168dc3717823cb38cf
Felipe Lopes <felipe.mlopes03@gmail.com>
2023-12-27 23:02:09 -0300

    feat: add uses cases in providers + DatabaseModule & CryptographyModule in imports

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -4,15 +4,26 @@ import { CreateAccountController } from './controllers/create-account.controller
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateQuestionController } from './controllers/create-question.controller'
 import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
-import { PrismaService } from '../database/prisma/prisma.service'
+import { DatabaseModule } from '../database/database.module'
+import { CreateQuestionUseCase } from 'src/domain/forum/application/use-cases/create-question'
+import { FetchRecentQuestionsUseCase } from 'src/domain/forum/application/use-cases/fetch-recent-questions'
+import { RegisterStudentUseCase } from 'src/domain/forum/application/use-cases/register-student'
+import { AuthenticateStudentUseCase } from 'src/domain/forum/application/use-cases/authenticate-student'
+import { CryptographyModule } from '../cryptography/cryptography.module'
 
 @Module({
+  imports: [DatabaseModule, CryptographyModule],
   controllers: [
     CreateAccountController,
     AuthenticateController,
     CreateQuestionController,
     FetchRecentQuestionsController,
   ],
-  providers: [PrismaService],
+  providers: [
+    CreateQuestionUseCase,
+    FetchRecentQuestionsUseCase,
+    RegisterStudentUseCase,
+    AuthenticateStudentUseCase,
+  ],
 })
 export class HttpModule {}
