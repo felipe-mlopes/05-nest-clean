cf9042c97ab8262c620b3cc5274066fdd258ed1c
Felipe Lopes <felipe.mlopes03@gmail.com>
2023-12-29 22:48:34 -0300

    feat: create Fetch Question Answers controller

---
+++ src/infra/http/controllers/fetch-question-anwsers.controller.ts
@@ -0,0 +1,49 @@
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Param,
+  Query,
+} from '@nestjs/common'
+import { z } from 'zod'
+
+import { ZodValidationPipe } from '../pipes/zod-validation-pipe'
+import { FetchQuestionAnswersUseCase } from 'src/domain/forum/application/use-cases/fetch-question-answers'
+import { AnswerPresenter } from 'src/infra/presenters/answer-presenter'
+
+const pageQueryParamsSchema = z
+  .string()
+  .optional()
+  .default('1')
+  .transform(Number)
+  .pipe(z.number().min(1))
+
+const queryValidationPipe = new ZodValidationPipe(pageQueryParamsSchema)
+
+type PageQueryParamsSchema = z.infer<typeof pageQueryParamsSchema>
+
+@Controller('/questions/:questionId/answers')
+export class FetchQuestionAnswersController {
+  constructor(private fetchQuestionAnswers: FetchQuestionAnswersUseCase) {}
+
+  @Get()
+  async handle(
+    @Query('page', queryValidationPipe) page: PageQueryParamsSchema,
+    @Param('questionId') questionId: string,
+  ) {
+    const result = await this.fetchQuestionAnswers.execute({
+      page,
+      questionId,
+    })
+
+    if (result.isLeft()) {
+      return new BadRequestException()
+    }
+
+    const answers = result.value.answers
+
+    return {
+      answers: answers.map((answer) => AnswerPresenter.toHTTP(answer)),
+    }
+  }
+}
