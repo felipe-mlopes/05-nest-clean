408fb68b50d7e26f9bded54b864136be64176969
Felipe Lopes <felipe.mlopes03@gmail.com>
2023-12-29 22:46:18 -0300

    feat: add many controllers routes in controllers & many use cases in providers

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,15 +1,35 @@
 import { Module } from '@nestjs/common'
 
+import { DatabaseModule } from '../database/database.module'
+import { CryptographyModule } from '../cryptography/cryptography.module'
+
 import { CreateAccountController } from './controllers/create-account.controller'
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateQuestionController } from './controllers/create-question.controller'
 import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
-import { DatabaseModule } from '../database/database.module'
+import { GetQuestionBySlugController } from './controllers/get-question-by-slug.controller'
+import { EditQuestionController } from './controllers/edit-question.controller'
+import { DeleteQuestionController } from './controllers/delete-question.controller'
+import { AnswerQuestionController } from './controllers/answer-question.controller'
+import { EditAnswerController } from './controllers/edit-answer.controller'
+import { DeleteAnswerController } from './controllers/delete-answer.controller'
+import { FetchQuestionAnswersController } from './controllers/fetch-question-anwsers.controller'
+import { ChooseQuestionBestAnswerController } from './controllers/choose-question-best-answer.controller'
+import { CommentOnQuestionController } from './controllers/comment-on-question.controller'
+
 import { CreateQuestionUseCase } from 'src/domain/forum/application/use-cases/create-question'
 import { FetchRecentQuestionsUseCase } from 'src/domain/forum/application/use-cases/fetch-recent-questions'
 import { RegisterStudentUseCase } from 'src/domain/forum/application/use-cases/register-student'
 import { AuthenticateStudentUseCase } from 'src/domain/forum/application/use-cases/authenticate-student'
-import { CryptographyModule } from '../cryptography/cryptography.module'
+import { GetQuestionBySlugUseCase } from 'src/domain/forum/application/use-cases/get-question-by-slug'
+import { EditQuestionUseCase } from 'src/domain/forum/application/use-cases/edit-question'
+import { DeleteQuestionUseCase } from 'src/domain/forum/application/use-cases/delete-question'
+import { AnswerQuestionUseCase } from 'src/domain/forum/application/use-cases/answer-question'
+import { EditAnswerUseCase } from 'src/domain/forum/application/use-cases/edit-answer'
+import { DeleteAnswerUseCase } from 'src/domain/forum/application/use-cases/delete-answer'
+import { FetchQuestionAnswersUseCase } from 'src/domain/forum/application/use-cases/fetch-question-answers'
+import { ChooseQuestionBestAnswerUseCase } from 'src/domain/forum/application/use-cases/choose-question-best-answer'
+import { CommentOnQuestionUseCase } from 'src/domain/forum/application/use-cases/comment-on-question'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
@@ -18,12 +38,30 @@ import { CryptographyModule } from '../cryptography/cryptography.module'
     AuthenticateController,
     CreateQuestionController,
     FetchRecentQuestionsController,
+    GetQuestionBySlugController,
+    EditQuestionController,
+    DeleteQuestionController,
+    AnswerQuestionController,
+    EditAnswerController,
+    DeleteAnswerController,
+    FetchQuestionAnswersController,
+    ChooseQuestionBestAnswerController,
+    CommentOnQuestionController,
   ],
   providers: [
     CreateQuestionUseCase,
     FetchRecentQuestionsUseCase,
     RegisterStudentUseCase,
     AuthenticateStudentUseCase,
+    GetQuestionBySlugUseCase,
+    EditQuestionUseCase,
+    DeleteQuestionUseCase,
+    AnswerQuestionUseCase,
+    EditAnswerUseCase,
+    DeleteAnswerUseCase,
+    FetchQuestionAnswersUseCase,
+    ChooseQuestionBestAnswerUseCase,
+    CommentOnQuestionUseCase,
   ],
 })
 export class HttpModule {}
