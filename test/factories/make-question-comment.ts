5d17021bfaf8c0b1de7e739878fb94d09ca888de
Felipe Lopes <felipe.mlopes03@gmail.com>
2023-12-29 22:50:47 -0300

    test: add Question Comment Factory class

--- test/factories/make-question-comment.ts
+++ test/factories/make-question-comment.ts
@@ -1,9 +1,14 @@
+import { faker } from '@faker-js/faker'
+import { Injectable } from '@nestjs/common'
+
 import { UniqueEntityID } from 'src/core/entities/unique-entity-id'
 import {
   QuestionComment,
   QuestionCommentProps,
 } from 'src/domain/forum/enterprise/entities/question-comment'
-import { faker } from '@faker-js/faker'
+
+import { PrismaService } from 'src/infra/database/prisma/prisma.service'
+import { PrismaQuestionCommentMapper } from 'src/infra/database/prisma/mappers/prisma-question-comment-mapper'
 
 export function makeQuestionComment(
   override: Partial<QuestionCommentProps> = {},
@@ -20,4 +25,21 @@ export function makeQuestionComment(
   )
 
   return questionComment
+}
+
+@Injectable()
+export class QuestionCommentFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaQuestionComment(
+    data: Partial<QuestionCommentProps> = {},
+  ): Promise<QuestionComment> {
+    const questionComment = makeQuestionComment(data)
+
+    await this.prisma.comment.create({
+      data: PrismaQuestionCommentMapper.toPrisma(questionComment),
+    })
+
+    return questionComment
+  }
 }
