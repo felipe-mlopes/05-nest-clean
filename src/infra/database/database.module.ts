64368b429da08721c77fea42541dc8344ac2c3d5
Felipe Lopes <felipe.mlopes03@gmail.com>
2023-12-31 19:12:54 -0300

    feat: add AttachmentsRepository in exports

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,20 +1,24 @@
 import { Module } from '@nestjs/common'
 
 import { PrismaService } from './prisma/prisma.service'
+
 import { PrismaQuestionsRepository } from './prisma/repositories/prisma-questions-repository'
 import { PrismaQuestionCommentsRepository } from './prisma/repositories/prisma-question-comments-repository'
-import { PrismaQuestionAttachmentRepository } from './prisma/repositories/prisma-question-attachment-repository'
+import { PrismaQuestionAttachmentsRepository } from './prisma/repositories/prisma-question-attachments-repository'
 import { PrismaAnswersRepository } from './prisma/repositories/prisma-answers-repository'
 import { PrismaAnswerCommentsRepository } from './prisma/repositories/prisma-answer-comments-repository'
-import { PrismaAnswerAttachmentRepository } from './prisma/repositories/prisma-answer-attachment-repository'
+import { PrismaAnswerAttachmentRepository } from './prisma/repositories/prisma-answer-attachments-repository'
+import { PrismaStudentsRepository } from './prisma/repositories/prisma-students-repository'
+import { PrismaAttachmentesRepository } from './prisma/repositories/prisma-attachments-repository'
+
 import { QuestionsRepository } from 'src/domain/forum/application/repositories/questions-repository'
 import { StudentsRepository } from 'src/domain/forum/application/repositories/students-repository'
-import { PrismaStudentsRepository } from './prisma/repositories/prisma-students-repository'
-import { AnswerAttachmentRepository } from 'src/domain/forum/application/repositories/answer-attachment-repository'
+import { AnswerAttachmentsRepository } from 'src/domain/forum/application/repositories/answer-attachments-repository'
 import { AnswerCommentsRepository } from 'src/domain/forum/application/repositories/answer-comments-repository'
 import { AnswersRepository } from 'src/domain/forum/application/repositories/answers-repository'
-import { QuestionAttachmentRepository } from 'src/domain/forum/application/repositories/question-attachment-repository'
+import { QuestionAttachmentsRepository } from 'src/domain/forum/application/repositories/question-attachments-repository'
 import { QuestionCommentsRepository } from 'src/domain/forum/application/repositories/question-comments-repository'
+import { AttachmentsRepository } from 'src/domain/forum/application/repositories/attachments-repository'
 
 @Module({
   providers: [
@@ -32,8 +36,8 @@ import { QuestionCommentsRepository } from 'src/domain/forum/application/reposit
       useClass: PrismaQuestionCommentsRepository,
     },
     {
-      provide: QuestionAttachmentRepository,
-      useClass: PrismaQuestionAttachmentRepository,
+      provide: QuestionAttachmentsRepository,
+      useClass: PrismaQuestionAttachmentsRepository,
     },
     {
       provide: AnswersRepository,
@@ -44,8 +48,12 @@ import { QuestionCommentsRepository } from 'src/domain/forum/application/reposit
       useClass: PrismaAnswerCommentsRepository,
     },
     {
-      provide: AnswerAttachmentRepository,
+      provide: AnswerAttachmentsRepository,
       useClass: PrismaAnswerAttachmentRepository,
+    },
+    {
+      provide: AttachmentsRepository,
+      useClass: PrismaAttachmentesRepository,
     },
   ],
   exports: [
@@ -53,10 +61,11 @@ import { QuestionCommentsRepository } from 'src/domain/forum/application/reposit
     StudentsRepository,
     QuestionsRepository,
     QuestionCommentsRepository,
-    QuestionAttachmentRepository,
+    QuestionAttachmentsRepository,
     AnswersRepository,
     AnswerCommentsRepository,
-    AnswerAttachmentRepository,
+    AnswerAttachmentsRepository,
+    AttachmentsRepository,
   ],
 })
 export class DatabaseModule {}
