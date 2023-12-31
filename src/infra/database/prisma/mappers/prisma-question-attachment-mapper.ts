890a437b201b12b962c662d6c7094f61a266e0c5
Felipe Lopes <felipe.mlopes03@gmail.com>
2023-12-31 19:13:49 -0300

    feat: add toPrismaUpdateMany method

--- src/infra/database/prisma/mappers/prisma-question-attachment-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-question-attachment-mapper.ts
@@ -1,4 +1,4 @@
-import { Attachment as PrismaQuestionAttachment } from '@prisma/client'
+import { Prisma, Attachment as PrismaQuestionAttachment } from '@prisma/client'
 import { UniqueEntityID } from 'src/core/entities/unique-entity-id'
 import { QuestionAttachment } from 'src/domain/forum/enterprise/entities/question-attachment'
 
@@ -15,5 +15,24 @@ export class PrismaQuestionAttachmentMapper {
       },
       new UniqueEntityID(raw.id),
     )
+  }
+
+  static toPrismaUpdateMany(
+    attachments: QuestionAttachment[],
+  ): Prisma.AttachmentUpdateManyArgs {
+    const attachmentIds = attachments.map((attachment) => {
+      return attachment.attachmentId.toString()
+    })
+
+    return {
+      where: {
+        id: {
+          in: attachmentIds,
+        },
+      },
+      data: {
+        questionId: attachments[0].questionId.toString(),
+      },
+    }
   }
 }
