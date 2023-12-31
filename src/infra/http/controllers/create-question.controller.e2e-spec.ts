4022b7c44df3fde13554754848925a4483595965
Felipe Lopes <felipe.mlopes03@gmail.com>
2023-12-31 19:39:14 -0300

    test: refactor the e2e test to add the attachments when creating a new question

--- src/infra/http/controllers/create-question.controller.e2e-spec.ts
+++ src/infra/http/controllers/create-question.controller.e2e-spec.ts
@@ -8,23 +8,26 @@ import { AppModule } from 'src/infra/app.module'
 
 import { StudentFactory } from 'test/factories/make-student'
 import { DatabaseModule } from 'src/infra/database/database.module'
+import { AttachmentFactory } from 'test/factories/make-attachment'
 
 describe('Create Question (E2E)', () => {
   let app: INestApplication
   let prisma: PrismaService
   let studentFactory: StudentFactory
+  let attachmentFactory: AttachmentFactory
   let jwt: JwtService
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule, DatabaseModule],
-      providers: [StudentFactory],
+      providers: [StudentFactory, AttachmentFactory],
     }).compile()
 
     app = moduleRef.createNestApplication()
 
     prisma = moduleRef.get(PrismaService)
     studentFactory = moduleRef.get(StudentFactory)
+    attachmentFactory = moduleRef.get(AttachmentFactory)
     jwt = moduleRef.get(JwtService)
 
     await app.init()
@@ -35,12 +38,16 @@ describe('Create Question (E2E)', () => {
 
     const accessToken = jwt.sign({ sub: user.id.toString() })
 
+    const attachment1 = await attachmentFactory.makePrismaAttachment()
+    const attachment2 = await attachmentFactory.makePrismaAttachment()
+
     const response = await request(app.getHttpServer())
       .post('/questions')
       .set('Authorization', `Bearer ${accessToken}`)
       .send({
         title: 'New question',
         content: 'Question content',
+        attachments: [attachment1.id.toString(), attachment2.id.toString()],
       })
 
     expect(response.statusCode).toBe(201)
@@ -52,5 +59,13 @@ describe('Create Question (E2E)', () => {
     })
 
     expect(questionOnDatabase).toBeTruthy()
+
+    const attachmentsOnDatabase = await prisma.attachment.findMany({
+      where: {
+        questionId: questionOnDatabase?.id,
+      },
+    })
+
+    expect(attachmentsOnDatabase).toHaveLength(2)
   })
 })
